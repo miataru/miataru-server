a97550ff37b98567e0d8d055c7bb7d0faca350ae
Daniel Kirstenpfad <bietiekay@gmail.com>
2013-08-23 21:53:30 +0200

    small changes and manual test run script

--- dev/TriggerUpdateLocation.sh
+++ dev/TriggerUpdateLocation.sh
@@ -1,3 +1,1 @@
-#!/bin/bash
-
 curl -H 'Content-Type: application/json' -X POST 'http://localhost:3000/UpdateLocation' -d '{"MiataruConfig":{"EnableLocationHistory":"False","LocationDataRetentionTime":"15"},"MiataruLocation":[{"Device":"7b8e6e0ee5296db345162dc2ef652c1350761823","Timestamp":"1376735651302","Longitude":"10.837502","Latitude":"49.828925","HorizontalAccuracy":"50.00"}]}'
---
+++ dev/run_tests.sh
@@ -0,0 +1,1 @@
+node ../node_modules/mocha/bin/mocha ../tests
