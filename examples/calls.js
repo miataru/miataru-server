a4240799730774208e5ab507d9de1f0eb422a8e1
zaphod1984 <zaphod84@gmx.de>
2013-08-26 23:40:30 +0200

    added examples

---
+++ examples/calls.js
@@ -0,0 +1,41 @@
+var OneMinute = {
+    "MiataruConfig":{
+        "EnableLocationHistory":"False",
+        "LocationDataRetentionTime":"1"
+    },
+    "MiataruLocation":[
+        {
+            "Device":"7b8e6e0ee5296db345162dc2ef652c1350761823",
+            "Timestamp":"1376735651302",
+            "Longitude":"10.837502",
+            "Latitude":"49.828925",
+            "HorizontalAccuracy":"50.00"
+        }
+    ]
+};
+
+var OneLocation = {
+    "MiataruConfig":{
+        "EnableLocationHistory":"True",
+        "LocationDataRetentionTime":"15"
+    },
+    "MiataruLocation":[
+        {
+            "Device":"7b8e6e0ee5296db345162dc2ef652c1350761823",
+            "Timestamp":"1376735651302",
+            "Longitude":"10.837502",
+            "Latitude":"49.828925",
+            "HorizontalAccuracy":"50.00"
+        }
+    ]
+};
+
+
+
+var getLocation = {
+    "MiataruGetLocation":[
+        {
+            "Device":"7b8e6e0ee5296db345162dc2ef652c1350761823"
+        }
+    ]
+};
