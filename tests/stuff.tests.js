e1a4885dd1d51e5f7f09591783d285318fdc0012
zaphod1984 <zaphod84@gmx.de>
2013-08-26 23:41:37 +0200

    added keybuilder test

---
+++ tests/stuff.tests.js
@@ -0,0 +1,17 @@
+var expect = require('chai').expect;
+
+var keyBuilder = require('../lib/utils/keyBuilder');
+
+describe('stuff', function() {
+
+    describe('keybuilder', function() {
+        it('should use only the main namespace', function() {
+            expect(keyBuilder.build()).to.equal('miad');
+        });
+
+        it('should use all the parameters to build a key', function() {
+            expect(keyBuilder.build('foo', 'bar')).to.equal('miad:foo:bar');
+        });
+    });
+
+});
