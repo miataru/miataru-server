bdf8388e29a1314538aadfcb977ceb3deeafd90e
zaphod1984 <zaphod84@gmx.de>
2013-08-27 00:11:39 +0200

    test breaker fixed

--- tests/dataHolder.tests.js
+++ tests/dataHolder.tests.js
@@ -1,12 +1,12 @@
 var expect = require('chai').expect;
 
-var Config = require('../lib/models/Location');
+var RequestConfig = require('../lib/models/RequestConfig');
 
 describe('dataHolders', function() {
 
     describe('config', function() {
         it('should throw because of invalid data', function() {
-            expect(function() { new Config() }).to.throw();
+            expect(function() { new RequestConfig() }).to.throw();
         });
     });
 
