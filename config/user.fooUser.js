f00394d013578fde1fb8fd6f20bf6965c2995baf
zaphod1984 <zaphod84@gmx.de>
2013-08-27 08:56:29 +0200

    added sample config

---
+++ config/user.fooUser.js
@@ -0,0 +1,11 @@
+//this is actually a example user config, it only extends (and propably overwrites some values)
+
+module.exports = {
+    database: {
+        type: 'real', //mock|real
+         host: 'localhost',
+         port: 6379,
+         userName: 'root',
+         password: ''
+    }
+};
