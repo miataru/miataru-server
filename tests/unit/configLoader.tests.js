4c3b4d0fbbcbc24aca38e5268a7adeaee45aa032
zaphod1984 <zaphod84@gmx.de>
2013-08-30 13:20:11 +0200

    cleanup

--- tests/unit/configLoader.tests.js
+++ tests/unit/configLoader.tests.js
@@ -69,7 +69,7 @@ describe('configLoader', function() {
         expect(config).to.deep.equal({foo:'bar', bax: 'baz', a: {b: {c: 256, d: 1024 } }});
     });
 
-    it('should throw an invalid external file', function() {
+    it('should throw on invalid external file', function() {
         expect(function() {
             configLoader.load(path.join(TEST_FILE_BASE, 'config'), {externalconfig: 'fooooooBar'});
         }).to.throw();
