56f9e606180a464a92d465df79f25c73b6d89545
Daniel Kirstenpfad <bietiekay@gmail.com>
2025-08-21 14:53:34 +0200

    Validate EnableLocationHistory types

--- tests/unit/dataHolder.tests.js
+++ tests/unit/dataHolder.tests.js
@@ -8,6 +8,24 @@ describe('dataHolders', function() {
         it('should throw because of invalid data', function() {
             expect(function() { new RequestConfig() }).to.throw();
         });
+
+        it('should throw when EnableLocationHistory is numeric', function() {
+            expect(function() {
+                new RequestConfig({
+                    EnableLocationHistory: 1,
+                    LocationDataRetentionTime: '15'
+                });
+            }).to.throw();
+        });
+
+        it('should throw when EnableLocationHistory is boolean', function() {
+            expect(function() {
+                new RequestConfig({
+                    EnableLocationHistory: true,
+                    LocationDataRetentionTime: '15'
+                });
+            }).to.throw();
+        });
     });
 
     describe('location', function() {
